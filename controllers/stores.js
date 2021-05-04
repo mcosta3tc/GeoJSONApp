49de04d04c6da01b2e950a00f2564b313038b67a
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-05-04 18:25:52 +0200

    back ended

--- controllers/stores.js
+++ controllers/stores.js
@@ -19,3 +19,27 @@ exports.getStores = async (req, res, next) => {
         });
     }
 }
+
+// @desc Create stores
+// @route POST /api/v1/stores
+// @access Public
+exports.addStores = async (req, res, next) => {
+    try {
+        const store = await Store.create(req.body);
+
+        return res.status(200).json({
+            success: true,
+            data: store
+        })
+    } catch (err) {
+        console.error(err);
+        if (err.code === 11000) {
+            return res.status(400).json({
+                error: 'This store already exists'
+            })
+        }
+        res.status(500).json({
+            error: 'Server error'
+        });
+    }
+}
--- models/Stores.js
+++ models/Stores.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const geocoder = require('../utils/geocoder');
 
 const StoreSchema = new mongoose.Schema({
     storeId: {
@@ -28,5 +29,23 @@ const StoreSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+//GeoCode & create location
+StoreSchema.pre('save', async function (next){
+    const loc = await geocoder.geocode(this.address)
+    console.log(loc);
+    this.location = {
+        type : 'Point',
+        coordinates: [
+            loc[0].longitude,
+            loc[0].latitude
+        ],
+        formattedAddress: loc[0].formattedAddress
+    }
+
+    //Do not save address
+    this.address = undefined;
+    next();
+})
 
 module.exports = mongoose.model('Store', StoreSchema);
